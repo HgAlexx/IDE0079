9826fef0137873a31953301335d63921aa94576c
HgAlexx <alex@mzhg.space>
2025-09-16 10:40:10 +0200

    Minimal repro

---
+++ IDE0079/ClassTest.cs
@@ -0,0 +1,14 @@
+﻿
+#pragma warning disable CA1000
+
+namespace IDE0079;
+
+public class ClassTest<T>
+{
+    public T? SomeMethod { get; init; }
+
+    public static void StaticTest()
+    {
+        throw new NotImplementedException();
+    }
+}
